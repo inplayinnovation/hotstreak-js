ce5ba4458ed19ae02a536ca27960df06ada5a4b9
Gianni Settino <g@miltonandparc.com>
2020-09-04 18:21:25 -0700

    Adds build script

---
+++ build.js
@@ -0,0 +1,7 @@
+const shell = require('shelljs');
+shell.exec('npm version patch');
+shell.exec(
+  './node_modules/browserify/bin/cmd.js src/hotstreak.js -p esmify --standalone HotStreak  > dist/web.js'
+);
+shell.exec('git add .');
+shell.exec('git commit -m "Made build"');
