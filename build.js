fa90f1b65a391b5b3745079b01fabc92f0d28116
Gianni Settino <g@miltonandparc.com>
2020-10-05 19:54:04 -0400

    Fixes publish script

--- build.js
+++
@@ -1,8 +0,0 @@
-const shell = require('shelljs');
-shell.exec('npm version patch');
-shell.exec(
-  './node_modules/browserify/bin/cmd.js src/hotstreak.js -p esmify --standalone HotStreak  > dist/web.js'
-);
-shell.exec('git add .');
-shell.exec('git commit -m "Made build"');
-shell.exec('npm publish');
